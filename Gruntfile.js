f1910dd90fece62107bcedca33a33eb01c69b3cf
Ivan <github@ivanhayes.com>
2014-07-29 23:45:33 +0100

    added Grunt workflow for minification of bullet.js.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+
+	"use strict";
+
+	grunt.initConfig({
+
+		/**
+		 * Watch
+		 * https://github.com/gruntjs/grunt-contrib-watch
+		 * Watches your scss, js etc for changes and compiles them
+		 */
+		watch : {
+
+			js: {
+				files: [
+					"src/js/libs/bullet.js"
+				],
+
+				tasks: ["uglify"]
+			}
+		},
+
+		uglify : {
+
+			dist : {
+				options : {
+					compress : true,
+					mangle : true,
+					sourceMap : true,
+					preserveComments : false,
+					report : "gzip"
+				},
+
+				src : ["src/js/libs/bullet.js"],
+				dest : "dist/bullet.min.js"
+			}
+		}
+	});
+
+
+	// Load all the grunt task modules.
+	require("load-grunt-tasks")(grunt, {pattern: ["grunt-*"]});
+
+	// =============
+	// === Tasks ===
+	// =============
+
+	// Register the default task for building the project.
+	grunt.registerTask("default", ["uglify:dist"]);
+};
--- src/js/libs/bullet.js
+++ src/js/libs/bullet.js
@@ -6,9 +6,9 @@
 
 		_self.on = function (event, fn, once)
 		{
-			if (arguments.length < 2
-				|| typeof event !== "string"
-				|| typeof fn !== "function") return;
+			if (arguments.length < 2 ||
+				typeof event !== "string" ||
+				typeof fn !== "function") return;
 
 			var fnString = fn.toString();
 
@@ -43,12 +43,12 @@
 		_self.once = function (event, fn)
 		{
 			_self.on(event, fn, true);
-		}
+		};
 
 		_self.off = function (event, fn)
 		{
-			if (typeof event !== "string"
-				|| typeof _events[event] === "undefined") return;
+			if (typeof event !== "string" ||
+				typeof _events[event] === "undefined") return;
 
 			// remove just the function, if passed as a parameter and in the dictionary.
 			if (typeof fn === "function")
@@ -68,12 +68,12 @@
 				// registered to this event by deleting the named event object.
 				delete _events[event];
 			}
-		}
+		};
 
 		_self.trigger = function (event)
 		{
-			if (typeof event !== "string"
-				|| typeof _events[event] === "undefined") return;
+			if (typeof event !== "string" ||
+				typeof _events[event] === "undefined") return;
 
 			for (var fnString in _events[event].callbacks)
 			{
@@ -82,7 +82,7 @@
 				if (typeof callbackObject.cb === "function") callbackObject.cb();
 				if (typeof callbackObject.once === "boolean" && callbackObject.once === true) _self.off(event, callbackObject.cb);
 			}
-		}
+		};
 
 	};
 
