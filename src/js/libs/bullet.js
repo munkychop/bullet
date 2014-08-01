8ce4b96d32fea98531541c4c4a49c01b2a1c89eb
Ivan Hayes <github@ivanhayes.com>
2014-08-01 16:53:50 +0100

    added AMD/module support.

--- src/js/libs/bullet.js
+++ src/js/libs/bullet.js
@@ -86,5 +86,24 @@
 
 	};
 
-	window.Bullet = new BulletClass();
+	// check for AMD/Module support, otherwise define SwiftClick as a global variable.
+	if (typeof define !== "undefined" && define.amd)
+	{
+		// AMD. Register as an anonymous module.
+		define (function()
+		{
+			"use strict";
+			return new BulletClass();
+		});
+
+	}
+	else if (typeof module !== "undefined" && module.exports)
+	{
+		module.exports = new BulletClass();
+	}
+	else
+	{
+		window.Bullet = new BulletClass();
+	}
+	
 })();
